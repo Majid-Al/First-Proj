e1e132d1ff8111b6854c8aae6e56c82e4d4fa2b0
Majid <mi.aliyan@gmail.com>
2025-05-19 10:59:48 +0330

    save commit

--- Mafia/Assets/Scripts/Panels-Popups/PopupTimer.cs
+++ Mafia/Assets/Scripts/Panels-Popups/PopupTimer.cs
@@ -14,29 +14,18 @@ public class PopupTimer : MonoBehaviour
 
     [SerializeField]
     AudioSource textAudioSource;
-    bool inProgress= false;
+
     // gets called from buttons in the panel
     public void StartCustomCountDown(int time)
     {
-        if (time == 0 && inputField != null && int.TryParse(inputField.text, out int result) && !inProgress)
-        {
-            inProgress = true;
-            int countdownTime = result;
-            StartCoroutine(CountdownCoroutine(countdownTime));
-        }
-        else if (time > 0 && !inProgress)
-        {
-            inProgress = true;
-            StartCoroutine(CountdownCoroutine(time));
-        }
-
+        StopAllCoroutines();
+        StartCoroutine(CountdownCoroutine(time));
     }
-
 
 
     private IEnumerator CountdownCoroutine(int time)
     {
-        while (time > 0)
+        while (time > 0 )
         {
             System.TimeSpan newTime = System.TimeSpan.FromSeconds(time);
             text.text = newTime.ToString(@"mm\:ss");
@@ -44,7 +33,6 @@ public class PopupTimer : MonoBehaviour
             time--;
         }
         text.text = "00";
-        inProgress = false;
         textAudioSource.Play();
 
     }
